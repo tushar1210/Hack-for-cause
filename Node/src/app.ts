89fc2e7a913266a7bf55a734a524fbe63be03a3b
tushar.1210 <ts26192271@gmail.com>
2020-03-12 05:59:01 +0530

    fix cors

--- Node/src/app.ts
+++ Node/src/app.ts
@@ -14,6 +14,12 @@ mongoose.set('useCreateIndex', true) ;
 mongoose.set('useFindAndModify', false);
 // mongoose.Promise = global.Promise ;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
